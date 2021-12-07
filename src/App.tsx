c7e9607f5e6e4d2ac00580350ed8b66d62d59c88
Amanda <amandaduuaarte@gmail.com>
2021-12-07 17:45:33 -0300

    Criação do estilo global da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { GlobalStyle } from "./assets/styles/global";
 
 
 export function App() {
   return (
     <div className="App">
      <h1> hello world! </h1>
+     <GlobalStyle/>
     </div>
   );
 }
