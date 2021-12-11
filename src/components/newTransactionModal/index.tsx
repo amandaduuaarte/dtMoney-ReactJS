6688c78ff04d9ab09993722a27b39eb20f50e590
Amanda <amandaduuaarte@gmail.com>
2021-12-11 14:35:57 -0300

    Mudança nas cores do botã

--- src/components/newTransactionModal/index.tsx
+++ src/components/newTransactionModal/index.tsx
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { Container, TransactionTypeContainer } from './styles';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 
 interface NewTransactionModalProps{
@@ -10,6 +11,8 @@ interface NewTransactionModalProps{
   onRequestClose: ()=> void //Function que não retorna nada;
 }
 export function NewTransactionModal ({ isOpen, onRequestClose}:NewTransactionModalProps) {
+
+  const [type, setType] = useState('deposit');
   return(
     <Modal 
     isOpen={isOpen} 
@@ -30,16 +33,22 @@ export function NewTransactionModal ({ isOpen, onRequestClose}:NewTransactionMod
       placeholder="Valor"/>
 
         <TransactionTypeContainer>
-          <button 
-          type="button">
+          <RadioBox 
+          type="button"
+          onClick={()=>{setType('deposit')}}
+          isActive={type=='deposit'}
+          activeColor="green">
           <img src={incomeImg} alt ="entrada"/>
           <span>Entrada</span>
-          </button>
-          <button 
-          type="button">
+          </RadioBox>
+          <RadioBox 
+          type="button"
+          onClick={()=>{setType('withdraw')}}
+          isActive={type=='withdraw'}
+          activeColor="red">
           <img src={outcomeImg} alt ="saida"/>
-          <span>Entrada</span>
-          </button>
+          <span>Saída</span>
+          </RadioBox>
         </TransactionTypeContainer>
       <input 
       placeholder="Categoria"/>
--- src/components/newTransactionModal/styles.ts
+++ src/components/newTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {darken, transparentize} from 'polished'
 
 export const Container = styled.form`
    h2{
@@ -51,15 +52,35 @@ export const TransactionTypeContainer = styled.div`
   grid-template-columns: 1fr 1fr;
   gap:0.5rem;
 
-  button{
-     height:4rem;
+  }
+`;
+
+interface RadioBoxProps{
+  isActive:boolean;
+  activeColor: 'green'| 'red';
+}
+
+const colors ={
+  green: '#33cc95',
+  red: '#E52E4D'
+}
+export const RadioBox = styled.button<RadioBoxProps>` //pasar props no styled
+  height:4rem;
      border:1px solid #d7d7d7;
      border-radius: 0.25rem;
-     background:transparent;
+     background:${(props)=> 
+     props.isActive ?transparentize(0.8, colors[props.activeColor]):
+     'trasnparent'};
 
      display:flex;
      align-items:center;
      justify-content:center;
+
+     transition: border-color 0.2s;
+
+     &:hover{
+       border-color: ${darken(0.1, '#d7d7d7')};
+     }
 
      img {
        height:20px;
@@ -69,6 +90,4 @@ export const TransactionTypeContainer = styled.div`
      span{
        display:inline-block; //pra dar margin nele 
        margin-left:1rem;
-     }
-  }
 `;
